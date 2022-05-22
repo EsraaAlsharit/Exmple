cc4da1bb6dcc0f22e95b472317a6e3eb9229bea8
esraa alsharit <esraa.alsharit@gmail.com>
2022-05-23 01:15:07 +0300

    Create Variables assignment

---
+++ Create Variables/CreateVariables.js
@@ -0,0 +1,10 @@
+var tall = 63, age = 25;
+
+//the tall must be more 42 or equal
+//age have to be more 10
+
+if (tall >= 42 && age > 10) {
+    console.log("you can ride the rollercoaster");
+} else {
+    console.log("you can't ride the rollercoaster");
+}

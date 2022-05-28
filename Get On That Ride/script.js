0712c75c90699d8f6936af8248d1713578446d05
esraa alsharit <esraa.alsharit@gmail.com>
2022-05-28 22:12:45 +0300

    with insert

--- Get On That Ride/script.js
+++ Get On That Ride/script.js
@@ -1,7 +1,34 @@
-let tall= 62;
-//tall= readInt(tall);
-if(tall>52){
-    console.log("Get on that ride, kiddo!");
-}else{
-    console.log("Sorry kiddo. Maybe next year.");
-}
+// let tall = 62;
+
+// if (tall > 52) {
+//     console.log("Get on that ride, kiddo!");
+// } else {
+//     console.log("Sorry kiddo. Maybe next year.");
+// }
+
+//with insert
+
+const readline = require('readline');
+
+const inquirer = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+inquirer.question("How old are you?", age => {
+    if (age > 52) {
+        console.log("Get on that ride, kiddo!");
+    } else {
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+    //    console.log(`${name} is ${age} years old`);
+    inquirer.close();
+    process.exit(0);
+});
+
+// inquirer.on("close", function() {
+//     console.log("Good bye!");
+//
+// });
+
+
---
+++ Get On That Ride/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+inquirer.on("close", function() {
+//     console.log("Good bye!");
+//     process.exit(0);
+// });

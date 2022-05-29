53e7a7830be11531f8df736cda4d6a01c772d266
esraa alsharit <esraa.alsharit@gmail.com>
2022-05-29 16:11:42 +0300

    md

--- Parameters/mycode/script.js
+++ Parameters/mycode/script.js
@@ -1,8 +1,10 @@
+const d = new Date();
+let time = d.getHours() + ":" + d.getMinutes();
 function greet(name) {
     if (name == "Count Dooku") {
         return "I'm coming for you, Dooku!";
     }
-    return "Good day, " + name + "!";
+    return `\n ${time} Good day, " ${name} !"`;
 }
 var greeting = greet("Anakin");
 console.log(greeting);

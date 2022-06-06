2750899e2cf57da8d3096de128797047a79b7434
esraa alsharit <esraa.alsharit@gmail.com>
2022-06-06 11:22:56 +0300

    Functions Intermediate I

---
+++ Functions Intermediate I/mycode/FunctionsIntermediate1.py
@@ -0,0 +1,18 @@
+
+
+import random
+# rand = random.random()
+# print(rand)
+def randInt(min=  0 , max=  100 ):
+    if max > min:
+        num = random.random()*max+min
+        return round (num)
+    else:
+        
+        num = random.random()*100
+        return round (num)
+
+print(randInt())                     # should print a random integer between 0 to 100
+print(randInt(max=50))                 # should print a random integer between 0 to 50
+print(randInt(min=50))                 # should print a random integer between 50 to 100
+print(randInt(min=50, max=10))      # should print a random integer between 50 and 500

3995f6747f079e3870bdc785d55ed049f9181c09
esraa alsharit <esraa.alsharit@gmail.com>
2022-06-08 02:13:42 +0300

    mathdojo

--- MathDojo/mycode/mathdojo.py
+++ MathDojo/mycode/mathdojo.py
@@ -23,4 +23,7 @@ md = MathDojo()
 x = md.add(2).add(2,5,1).subtract(3,2).result
 print(x)	# should print 5
 # run each of the methods a few more times and check the result!
+op1 = MathDojo()
+print(op1.add(7).add(4,6,1).add(2).result)
+print(op1.subtract(2).subtract(4,3,1).subtract(2).result)
 

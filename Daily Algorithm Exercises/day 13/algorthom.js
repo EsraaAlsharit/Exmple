6b3fb668152f98f569dc861ef662b63b85b9820f
esraa alsharit <esraa.alsharit@gmail.com>
2022-06-09 10:48:20 +0300

    algorithm

---
+++ Daily Algorithm Exercises/day 13/algorthom.js
@@ -0,0 +1,50 @@
+/*
+  Zip Arrays into Map
+  Given two arrays, create an associative array (aka hash map, an obj / dictionary) containing keys from the first array, and values from the second.
+  Associative arrays are sometimes called maps because a key (string) maps to a value
+ */
+
+const keys1 = ["abc", 3, "yo"]
+const vals1 = [42, "wassup", true]
+// const expected1 = {
+//   abc: 42,
+//   3: "wassup",
+//   yo: true,
+// }
+
+function zipArraysIntoMap(keys, values) {
+    // code here
+    dic={}
+    for (let index = 0; index < keys.length; index++) {
+        dic[keys[index]]=values[index]
+    }
+    return dic
+}
+console.log(zipArraysIntoMap(keys1,vals1))
+/*****************************************************************************/
+
+/*
+    Invert Hash
+    A hash table / hash map is an obj / dictionary
+    Given an object / dict,
+    return a new object / dict that has the keys and the values swapped so that the keys become the values and the values become the keys
+  */
+const obj1 = {
+    name: "Zaphod",
+    charm: "high",
+    morals: "dicey"
+};
+  // const expected1 = {
+  //   Zaphod: "name",
+  //   high: "charm",
+  //   dicey: "morals"
+  // };
+function invertObj(obj) {
+    // code here
+    dic={}
+    for (var i in obj){
+        dic[obj[i]]=i
+    }
+    return dic
+}
+console.log(invertObj(obj1))

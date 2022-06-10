849819063e40c548540e92f238a602fdbcdcfbb8
esraa alsharit <esraa.alsharit@gmail.com>
2022-06-10 20:05:49 +0300

    Zoo

---
+++ Zoo/mycode/Animal.py
@@ -0,0 +1,13 @@
+from curses import ACS_GEQUAL
+from unicodedata import name
+
+
+class Animal:
+    def __init__(self):
+        self.name=name
+        self.age= age
+        self.health= health
+        self.happiness = happy
+    
+    def display_info():
+        pass

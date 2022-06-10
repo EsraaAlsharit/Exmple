091f358663de7f21d236268dc07d352a60d89a09
esraa alsharit <esraa.alsharit@gmail.com>
2022-06-11 02:41:43 +0300

    dojo survey

--- Dojo Survey/Dojo_Survey/Dojo_Survey_App/views.py
+++ Dojo Survey/Dojo_Survey/Dojo_Survey_App/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+
+def result(request):
+    print("Got Post Info....................")
+    name_from_form = request.POST['name']
+    loc_from_form = request.POST['loc']
+    lang_from_form = request.POST['lang']
+    comm_from_form = request.POST['comm']
+
+    print(name_from_form)
+    print(loc_from_form)
+    print(comm_from_form)
+    print(lang_from_form)
+    
+    context = {
+        "name": name_from_form,
+        "location": loc_from_form,
+        "language": lang_from_form,
+        "commit": comm_from_form
+    }
+    return render(request, "result.html", context)

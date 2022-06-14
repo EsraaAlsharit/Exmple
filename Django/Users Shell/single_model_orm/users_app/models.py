87a2ac6034a02badd45b9839b6ad9d513af71531
esraa alsharit <esraa.alsharit@gmail.com>
2022-06-14 16:18:48 +0300

    users shell

--- Django/Users Shell/single_model_orm/users_app/models.py
+++ Django/Users Shell/single_model_orm/users_app/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 # models is DB in django
-    
+
+
 class users(models.Model):
-    #auto build id field
-    first_name =models.CharField(max_length=255)
+    # auto build id field
+    first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email_address = models.CharField(max_length=255)
     age = models.IntegerField()
     create_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __repr__(self):
+        return "first_name: {}".format(self.first_name)
+
+    def __str__(self):
+        return f"<Person object:\nid: ({self.id})\nname: {self.name}\nbirth date: {self.birthDate}\nnumber: {self.number} >"

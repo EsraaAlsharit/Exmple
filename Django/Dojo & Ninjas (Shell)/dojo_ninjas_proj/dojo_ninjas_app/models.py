e7f663d51be8e32acca2dd6dfe4b010c8c3f3dda
esraa alsharit <esraa.alsharit@gmail.com>
2022-06-15 11:37:17 +0300

    shell

--- Django/Dojo & Ninjas (Shell)/dojo_ninjas_proj/dojo_ninjas_app/models.py
+++ Django/Dojo & Ninjas (Shell)/dojo_ninjas_proj/dojo_ninjas_app/models.py
@@ -5,6 +5,7 @@ class Dojos(models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=255)
+    desc=models.TextField(default='old dojo')
 class Ninja(models.Model):
     dojo = models.ForeignKey(Dojos, related_name="dojos", on_delete = models.CASCADE)
     fname = models.CharField(max_length=255)

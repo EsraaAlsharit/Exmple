56d5c85c5db12ebb0e130a813c8a47252acedf46
esraa alsharit <esraa.alsharit@gmail.com>
2022-06-24 18:19:33 +0300

    dojo servey bounes

--- Django/Dojo Survey/Dojo_Survey/Dojo_Survey_App/views.py
+++ Django/Dojo Survey/Dojo_Survey/Dojo_Survey_App/views.py
@@ -11,17 +11,27 @@ def result(request):
     # request._post[]
     loc_from_form = request.POST['loc']
     lang_from_form = request.POST['lang']
+    gander_from_form = request.POST['gander']
     comm_from_form = request.POST['comm']
+
+    online_from_form = ""
+    onsite_from_form = ""
+    if "onsite" in request.POST:
+        onsite_from_form = request.POST['onsite']
+    if "online" in request.POST:
+        online_from_form = request.POST['online']
 
     print(name_from_form)
     print(loc_from_form)
     print(comm_from_form)
     print(lang_from_form)
-    
+
     context = {
         "name": name_from_form,
         "location": loc_from_form,
         "language": lang_from_form,
-        "commit": comm_from_form
+        "gander": gander_from_form,
+        "commit": comm_from_form,
+        "work": [onsite_from_form, online_from_form]
     }
     return render(request, "result.html", context)

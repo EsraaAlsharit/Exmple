b2850bfd5317950d2cad23a0d01433d2f8300c98
esraa alsharit <esraa.alsharit@gmail.com>
2022-06-26 10:26:41 +0300

    bounes

--- Django/BooksAuthors with Templates/books_authors_pro/books_authors_app/models.py
+++ Django/BooksAuthors with Templates/books_authors_pro/books_authors_app/models.py
@@ -3,17 +3,21 @@ from django.db import models
 # Create your models here.
 
 
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    desc = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
 
 
 class Author(models.Model):
     fname = models.CharField(max_length=255)
     lname = models.CharField(max_length=255)
     notes = models.TextField(default="")
-    books = models.ManyToManyField(Book, related_name="books")
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    author = models.ManyToManyField(Author, related_name="books")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

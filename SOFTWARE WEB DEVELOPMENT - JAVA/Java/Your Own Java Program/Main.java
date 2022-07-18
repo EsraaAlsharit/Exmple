8485b71446662f713bf5b182f8a1296bbabd9075
esraa alsharit <esraa.alsharit@gmail.com>
2022-07-18 09:50:06 +0300

    java

---
+++ SOFTWARE WEB DEVELOPMENT - JAVA/Java/Your Own Java Program/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My name is Esraa Alsharit");
+        System.out.println("I am 25 years old");
+        System.out.println("My hometown is Alhassa");
+
+    }
+}

48c01df3924eb4f3955e52c607f69caf8ca509f3
esraa alsharit <esraa.alsharit@gmail.com>
2022-07-19 10:02:30 +0300

    Alfred

--- SOFTWARE WEB DEVELOPMENT - JAVA/Java/Alfred Bot/mycode/AlfredQuotes.java
+++ SOFTWARE WEB DEVELOPMENT - JAVA/Java/Alfred Bot/mycode/AlfredQuotes.java
@@ -1,31 +1,49 @@
 package mycode;
 
 import java.util.Date;
+
 public class AlfredQuotes {
-    
+
     public String basicGreeting() {
         // You do not need to code here, this is an example method
         return "Hello, lovely to see you. How are you?";
     }
-    
+
     public String guestGreeting(String name) {
         // YOUR CODE HERE
-        return "place holder for guest greeting return string";
+        return String.format("%s. Lovely to see you.", name);
+        // return "Greeting " + name;
     }
-    
-    public String dateAnnouncement() {
-        // YOUR CODE HERE
-        return "place holder for date announcement return string";
-    }
-    
-    public String respondBeforeAlexis(String conversation) {
-        // YOUR CODE HERE
-        return "for snarky response return string";
-    }
-    
-	// NINJA BONUS
-	// See the specs to overload the guessGreeting method
-    // SENSEI BONUS
-    // Write your own AlfredQuote method using any of the String methods you have learned!
-}
 
+    public String dateAnnouncement() {
+        // Date date = new Date();
+        // return "" + date;
+        return String.format("It is currently %s", new Date());
+    }
+
+    // public String respondBeforeAlexis(String conversation) {
+    //     // YOUR CODE HERE
+    //     return "for snarky response return string";
+    // }
+
+      // Inputs: String (Any phrase)
+    // Return Type: String
+    // Output: A repsonse (String)
+    public String answeringBeforeAlexis(String phrase) {
+
+        if(phrase.indexOf("Alexis") > -1) {
+            return "She's really of no help. What can I get for you?";
+        }
+
+        if (phrase.indexOf("Alfred") > -1) {
+            return "At your service, naturally. How may I be of assistance?";
+        }
+
+        return "Right. And with that I shall retire.";
+    }
+    // NINJA BONUS
+    // See the specs to overload the guessGreeting method
+    // SENSEI BONUS
+    // Write your own AlfredQuote method using any of the String methods you have
+    // learned!
+}
--- SOFTWARE WEB DEVELOPMENT - JAVA/Java/Alfred Bot/mycode/AlfredTest.java
+++ SOFTWARE WEB DEVELOPMENT - JAVA/Java/Alfred Bot/mycode/AlfredTest.java
@@ -15,23 +15,21 @@ public class AlfredTest {
         String testGuestGreeting = alfredBot.guestGreeting("Beth Kane");
         String testDateAnnouncement = alfredBot.dateAnnouncement();
         
-        String alexisTest = alfredBot.respondBeforeAlexis(
-                            "Alexis! Play some low-fi beats."
-                            );
-        String alfredTest = alfredBot.respondBeforeAlexis(
-            "I can't find my yo-yo. Maybe Alfred will know where it is.");
-        String notRelevantTest = alfredBot.respondBeforeAlexis(
-            "Maybe that's what Batman is about. Not winning. But failing.."
+        String alexisTest = alfredBot.answeringBeforeAlexis("Alexis! Play some low-fi beats.");
+        String alfredTest = alfredBot.answeringBeforeAlexis("I can't find my yo-yo. Maybe Alfred will know where it is.");
+        String notRelevantTest = alfredBot.answeringBeforeAlexis("Maybe that's what Batman is about. Not winning. But failing.."
         );
         
         // Print the greetings to test.
         System.out.println(testGreeting);
         
         // Uncomment these one at a time as you implement each method.
-        // System.out.println(testGuestGreeting);
-        // System.out.println(testDateAnnouncement);
-        // System.out.println(alexisTest);
-        // System.out.println(alfredTest);
-        // System.out.println(notRelevantTest);
+        System.out.println(testGuestGreeting);
+
+        System.out.println(testDateAnnouncement);
+
+        System.out.println(alexisTest);
+        System.out.println(alfredTest);
+        System.out.println(notRelevantTest);
     }
 }

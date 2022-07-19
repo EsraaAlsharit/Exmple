7ec9dcc104cc8166e89ba68fb856d0b44121ba58
esraa alsharit <esraa.alsharit@gmail.com>
2022-07-19 16:26:29 +0300

    BankAccount

---
+++ SOFTWARE WEB DEVELOPMENT - JAVA/Java/BankAccount/mycode/BankAccount.java
@@ -0,0 +1,23 @@
+package mycode;
+
+public class BankAccount {
+    double checkingbalance;
+    double savingsbalance;
+
+    public double getCheckingbalance() {
+        return checkingbalance;
+    }
+
+    public void setCheckingbalance(double checkingbalance) {
+        this.checkingbalance = checkingbalance;
+    }
+
+    public double getSavingsbalance() {
+        return savingsbalance;
+    }
+
+    public void setSavingsbalance(double savingsbalance) {
+        this.savingsbalance = savingsbalance;
+    }
+
+}

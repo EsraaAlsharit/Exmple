6dee3f0c64f54f23d7baa403012ca139ef502c3d
esraa alsharit <esraa.alsharit@gmail.com>
2022-07-21 10:20:30 +0300

    zoo 2

---
+++ SOFTWARE WEB DEVELOPMENT - JAVA/Java/Zoo Keeper Part 2/Zoo Keeper 2/src/Zoo/Bat.java
@@ -0,0 +1,27 @@
+package Zoo;
+
+public class Bat {
+	
+	public int energyLevel;
+
+	public Bat() {
+		// TODO Auto-generated constructor stub
+		this.energyLevel = 300;
+	}
+	
+	public void fly() {
+        this.energyLevel -= 50;
+        System.out.println("clicks clicks clicks clicks\n energy:" + this.energyLevel);
+    }
+
+    public void eatHumans() {
+        this.energyLevel += 25;
+        System.out.println("so- well\n energy:" + this.energyLevel);
+    }
+
+    public void attackTown() {
+        this.energyLevel -= 100;
+        System.out.println("town on fire fire!!\n energy:" + this.energyLevel);
+    }
+
+}

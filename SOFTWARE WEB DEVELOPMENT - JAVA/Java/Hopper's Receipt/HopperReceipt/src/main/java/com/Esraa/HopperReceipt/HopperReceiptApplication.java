45c176b18ce609cfb67093de0367fe6cf4bf6f15
esraa alsharit <esraa.alsharit@gmail.com>
2022-07-24 16:33:37 +0300

    Hopper's Receipt

---
+++ SOFTWARE WEB DEVELOPMENT - JAVA/Java/Hopper's Receipt/HopperReceipt/src/main/java/com/Esraa/HopperReceipt/HopperReceiptApplication.java
@@ -0,0 +1,38 @@
+package com.Esraa.HopperReceipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@SpringBootApplication
+public class HopperReceiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopperReceiptApplication.class, args);
+	}
+
+	// class definition and imports here...
+    @RequestMapping("/")
+    public String index(Model model) {
+        
+        String name = "Grace Hopper";
+        String itemName = "Copper wire";
+        double price = 5.25;
+        String description = "Metal strips, also an illustration of nanoseconds.";
+        String vendor = "Little Things Corner Store";
+    
+    	// Your code here! Add values to the view model to be rendered
+        model.addAttribute("name",name);
+        model.addAttribute("item",itemName);
+        model.addAttribute("price",price);
+        model.addAttribute("dec",description);
+        model.addAttribute("ven",vendor);
+    
+        return "index.jsp";
+    }
+    
+
+}

0322fae5fd8d56550a0bfce5d1db2eed67caa2c3
esraa alsharit <esraa.alsharit@gmail.com>
2022-07-24 14:03:38 +0300

    DaikichiPathVariables

---
+++ SOFTWARE WEB DEVELOPMENT - JAVA/Java/Daikichi Path Variables/DaikichiPathVariables/src/main/java/com/Esraa/DaikichiPathVariables/DaikichiPathVariablesApplication.java
@@ -0,0 +1,36 @@
+package com.Esraa.DaikichiPathVariables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DaikichiPathVariablesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DaikichiPathVariablesApplication.class, args);
+  }
+
+  @RequestMapping("/daikichi/travel/{country}")
+  public String travel(@PathVariable("country") String country) {
+    return "Congratulation! You will soon travel to " + country + "!";
+  }
+
+  @RequestMapping("/daikichi/lotto/{num}")
+  public String luck(@PathVariable("num") int num) {
+    if (num % 2 == 0) {
+      return "You will take a grand journey in the near future, but be weary of tempting offers";
+    } else {
+      return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+    }
+  }
+
+  // @RequestMapping("/")
+  // public String hello(){
+  // return "Hello";
+  // }
+
+}

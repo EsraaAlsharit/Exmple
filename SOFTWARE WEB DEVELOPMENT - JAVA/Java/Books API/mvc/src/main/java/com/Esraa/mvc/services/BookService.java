e2187195a80469d4296c86ac5d9788b07179e96e
esraa alsharit <esraa.alsharit@gmail.com>
2022-07-27 04:01:05 +0300

    Books API

---
+++ SOFTWARE WEB DEVELOPMENT - JAVA/Java/Books API/mvc/src/main/java/com/Esraa/mvc/services/BookService.java
@@ -0,0 +1,56 @@
+package com.Esraa.mvc.services;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import com.Esraa.mvc.models.Book;
+import com.Esraa.mvc.repositories.BookRepository;
+@Service
+public class BookService {
+ // adding the book repository as a dependency
+private final BookRepository bookRepository;
+ 
+ public BookService(BookRepository bookRepository) {
+     this.bookRepository = bookRepository;
+ }
+ // returns all the books
+ public List<Book> allBooks() {
+     return bookRepository.findAll();
+ }
+ // creates a book
+ public Book createBook(Book b) {
+     return bookRepository.save(b);
+ }
+ // retrieves a book
+ public Book findBook(Long id) {
+     Optional<Book> optionalBook = bookRepository.findById(id);
+     if(optionalBook.isPresent()) {
+         return optionalBook.get();
+     } else {
+         return null;
+     }
+ }
+public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+	// TODO Auto-generated method stub
+	Optional<Book> optionalBook = bookRepository.findById(id);
+	if(optionalBook.isPresent()) {
+//		optionalBook.id=id;
+		return null;
+		
+    } else {
+        return null;
+    }
+
+	
+}
+public void deleteBook(Long id) {
+	// TODO Auto-generated method stub
+	 bookRepository.deleteById(id);
+
+}
+
+}
+
+

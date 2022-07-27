45bed628c4be5c7ac0afbecc9c0dd639c72ca28e
esraa alsharit <esraa.alsharit@gmail.com>
2022-07-28 01:15:39 +0300

    update

--- SOFTWARE WEB DEVELOPMENT - JAVA/Java/Rendering Books/RenderingBooks/src/main/java/com/Esraa/RenderingBooks/controllers/BooksApi.java
+++ SOFTWARE WEB DEVELOPMENT - JAVA/Java/Rendering Books/RenderingBooks/src/main/java/com/Esraa/RenderingBooks/controllers/BooksApi.java
@@ -19,10 +19,10 @@ public class BooksApi {
  public BooksApi(BookService bookService){
      this.bookService = bookService;
  }
-// @RequestMapping("/api/books")
-// public List<Book> index() {
-//     return bookService.allBooks();
-// }
+ @RequestMapping("/api/books")
+ public List<Book> index() {
+     return bookService.allBooks();
+ }
  
  @RequestMapping(value="/api/books", method=RequestMethod.POST)
  public Book create(@RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {

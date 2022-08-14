ea83da579a5ba59be1f1762e45d766c593b979d5
esraa alsharit <esraa.alsharit@gmail.com>
2022-08-14 13:37:01 +0300

    Predict Destructuring

--- SOFTWARE WEB DEVELOPMENT - MERN/JavaScript/Predict Destructuring/script.js
+++ SOFTWARE WEB DEVELOPMENT - MERN/JavaScript/Predict Destructuring/script.js
@@ -1,16 +1,17 @@
 //Problem 1
-
+/*
 const cars = ['Tesla', 'Mercedes', 'Honda']
 const [ randomCar ] = cars
 const [ ,otherRandomCar ] = cars
 //Predict the output
 console.log(randomCar)
 console.log(otherRandomCar)
- 
+*/
+//Tesla
+//Mercedes
 
-console.log()
-// Problem 2
-
+//Problem 2
+/*
 const employee = {
     name: 'Elon',
     age: 47,
@@ -18,13 +19,13 @@ const employee = {
 }
 const { name: otherName } = employee;
 //Predict the output
-console.log(employee.name);
+console.log(name);
 console.log(otherName);
- 
+*/
+//name throw error 
 
-console.log()
 //Problem 3
-
+/*
 const person = {
     name: 'Phil Smith',
     age: 47,
@@ -34,26 +35,27 @@ const password = '12345';
 const { password: hashedPassword } = person;  
 //Predict the output
 console.log(password);
-console.log(hashedPassword);//undefined
- 
+console.log(hashedPassword);
+*/
+//12345 
+//undefined
 
-console.log()
 //Problem 4
-
+/*
 const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
 const [,first] = numbers;
 const [,,,second] = numbers;
 const [,,,,,,,,third] = numbers;
 //Predict the output
-first //2
-second //5
-third // 2
 console.log(first == second);
 console.log(first == third);
+*/
+//false
+// true
 
-console.log()
+
 //Problem 5
-
+/*
 const lastTest = {
     key: 'value',
     secondKey: [1, 5, 1, 8, 3, 3]
@@ -66,4 +68,9 @@ console.log(key);
 console.log(secondKey);
 console.log(secondKey[0]);
 console.log(willThisWork);
+*/
 
+//value
+//[1, 5, 1, 8, 3, 3]
+//1
+//5

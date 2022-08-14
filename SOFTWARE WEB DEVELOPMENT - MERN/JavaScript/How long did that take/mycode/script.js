f55ae264744e351c12344238b9cd6410b9917e3f
esraa alsharit <esraa.alsharit@gmail.com>
2022-08-14 14:53:28 +0300

    update

--- SOFTWARE WEB DEVELOPMENT - MERN/JavaScript/How long did that take/mycode/script.js
+++ SOFTWARE WEB DEVELOPMENT - MERN/JavaScript/How long did that take/mycode/script.js
@@ -1,4 +1,4 @@
-// //Calculating prime numbers
+// // //Calculating prime numbers
 
 // Number.prototype.isPrime = function () {
 //     for (let i = 2; i < this; i++) {
@@ -23,27 +23,37 @@
 // console.log(`This took ${performance.now() - start} milliseconds to run`);
 
 
-//Fibonacci
+// //Fibonacci
 
-// recursive
+// // recursive
 function rFib(n) {
     if (n < 2) {
         return n;
     }
     return rFib(n - 1) + rFib(n - 2);
 }
-rFib(20);
+console.log(rFib(20));
 // iterative
-function iFib(n) {
-    const vals = [0, 1];
-    while (vals.length - 1 < n) {
-        let len = vals.length;
-        vals.push(vals[len - 1] + vals[len - 2]);
-    }
-    return vals[n];
-}
-iFib(20);
+// function iFib(n) {
+//     const vals = [0, 1];
+//     while (vals.length - 1 < n) {
+//         let len = vals.length;
+//         vals.push(vals[len - 1] + vals[len - 2]);
+//     }
+//     return vals[n];
+// }
+// console.log(iFib(20));
 
 // //Reversing a string
-// const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
-// const reversed1 = story.split("").reverse().join("");
+const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
+const reversed1 = story.split("").reverse().join("");
+console.log(reversed1)
+
+console.log()
+var str=""
+for (let index = story.length-1; index > -1; index--) {
+    str+=story[index];
+    
+}
+
+// console.log(str)

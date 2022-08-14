d915173e29539814e48582ff55eee50b3aa308cc
esraa alsharit <esraa.alsharit@gmail.com>
2022-08-14 12:01:43 +0300

    Hoisting

--- SOFTWARE WEB DEVELOPMENT - MERN/JavaScript/Hoisting/script.js
+++ SOFTWARE WEB DEVELOPMENT - MERN/JavaScript/Hoisting/script.js
@@ -8,88 +8,119 @@
 
 // let example = "I'm the example!";
 // console.log(example);
+
 //1
-// var hello = 'world';
-// console.log(hello);
+/*
+console.log(hello);
+var hello = 'world';
+*/
+//undefined
+
 //2
-// var needle = 'haystack';
-// test();
-// function test() {
-//     var needle = 'magnet';
-//     console.log(needle);
-// }
+/*
+var needle = 'haystack';
+test();
+function test(){
+    var needle = 'magnet';
+    console.log(needle);
+}
+*/
+// haystack first than magnet
+
 //3
-// var brendan = 'super cool';
-// function print() {
-//     brendan = 'only okay';
-//     console.log(brendan);
-// }
-// console.log(brendan);//not working
+/*
+var brendan = 'super cool';
+function print(){
+    brendan = 'only okay';
+    console.log(brendan);
+}
+console.log(brendan);
+*/
+// 'super cool' only we did't call the function
 
 //4
-// var food = 'chicken';
-// console.log(food);
-// eat();
-// function eat() {
-//     var food = 'gone';
-//     console.log(food);
-//     food = 'half-chicken';
-//     console.log(food);
-// }
-//5
-// console.log(food);
-var mean = function () {
-    var food = "fish";
+/*
+var food = 'chicken';
+console.log(food);
+eat();
+function eat(){
+    food = 'half-chicken';
     console.log(food);
+    var food = 'gone';
+}
+*/
+
+// 'chicken' then 'half-chicken'
+
+//5
+/* mean();
+console.log(food);
+var mean = function() {
     food = "chicken";
     console.log(food);
+    var food = "fish";
+    console.log(food);
 }
-mean();
-//console.log(food);//not working
+console.log(food);
+*/
+//throw error it is not function it is value having function which need to defined
+//in line 56
+//mean();
+
 //6
-var genre = "disco";
+/*
 console.log(genre);
+var genre = "disco";
 rewind();
 function rewind() {
-    genre = "rock";
-    console.log(genre);
-    var genre = "r&b";
-    console.log(genre);
+    genre = "rock";
+    console.log(genre);
+    var genre = "r&b";
+    console.log(genre);
 }
 console.log(genre);
+*/
+//undefined
+//rock
+//r&b
+// disco
 
 //7
+/*
 dojo = "san jose";
 console.log(dojo);
 learn();
 function learn() {
-    dojo = "seattle";
-    console.log(dojo);
-    var dojo = "burbank";
-    console.log(dojo);
+    dojo = "seattle";
+    console.log(dojo);
+    var dojo = "burbank";
+    console.log(dojo);
 }
 console.log(dojo);
+*/
+//san jose
+//seattle
+//burbank
+//san jose
 
-//8
+// 8 - Bonus ES6: const
+/*
 console.log(makeDojo("Chicago", 65));
 console.log(makeDojo("Berkeley", 0));
 function makeDojo(name, students){
-    var dojo = {};
-    dojo.name = name;
-    dojo.students = students;
-    if(dojo.students > 50){
-        dojo.hiring = true;
-    }
-    else if(dojo.students <= 0){
-        dojo = "closed for now";
-    }
-    return dojo;
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if(dojo.students > 50){
+        dojo.hiring = true;
+    }
+    else if(dojo.students <= 0){
+        dojo = "closed for now";
+    }
+    return dojo;
 }
-
-
-
-
-
+*/
+// because const need to be var to change values
 
 
 

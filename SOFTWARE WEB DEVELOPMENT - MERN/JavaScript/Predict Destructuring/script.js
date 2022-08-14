25e8755fefb4fda4bd668472c8265844155241b2
esraa alsharit <esraa.alsharit@gmail.com>
2022-08-14 11:16:42 +0300

    Predict Destructuring

---
+++ SOFTWARE WEB DEVELOPMENT - MERN/JavaScript/Predict Destructuring/script.js
@@ -0,0 +1,69 @@
+//Problem 1
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+ 
+
+console.log()
+// Problem 2
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(employee.name);
+console.log(otherName);
+ 
+
+console.log()
+//Problem 3
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);//undefined
+ 
+
+console.log()
+//Problem 4
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+first //2
+second //5
+third // 2
+console.log(first == second);
+console.log(first == third);
+
+console.log()
+//Problem 5
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+

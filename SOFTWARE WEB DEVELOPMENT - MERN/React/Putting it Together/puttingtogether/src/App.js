20010e79a787567cc5cd8148a92df068eea81c9e
esraa alsharit <esraa.alsharit@gmail.com>
2022-08-18 03:11:25 +0300

    Putting it Together

---
+++ SOFTWARE WEB DEVELOPMENT - MERN/React/Putting it Together/puttingtogether/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+import FirstComponent from './components/FirstComponent';
+
+function App() {
+  return (
+    
+      <>
+      
+      <FirstComponent fname="Doe" lname="Jane" HairColor="Black" age={45} />
+      <FirstComponent fname="Smith" lname="John" HairColor="Brown" age={88} />
+      
+      </>
+    
+  );
+}
+
+export default App;
---
+++ SOFTWARE WEB DEVELOPMENT - MERN/React/Putting it Together/puttingtogether/src/components/FirstComponent.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class FirstComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            position: false
+        }
+    }
+
+    Birthday = () => {
+        console.log(this.setState.age = this.props.age + 1)
+        // this.setState(this.age += 1);
+    }
+
+    render() {
+
+        const { fname, lname, HairColor, age } = this.props
+        return (
+            <>
+                <div className=''>
+                    <h1>{fname}, {lname}</h1>
+                    <p>Age: {age}</p>
+                    <p>Hair Color:{HairColor}</p>
+                </div>
+                <button onClick={this.Birthday}>Birthday Button for {lname} {fname}</button>
+            </>
+        );
+    }
+
+}
+export default FirstComponent;

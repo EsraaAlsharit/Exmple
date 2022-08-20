193aceefed50d15b6932427b1ca97f75e886dcf9
esraa alsharit <esraa.alsharit@gmail.com>
2022-08-20 22:07:50 +0300

    more from

--- SOFTWARE WEB DEVELOPMENT - MERN/React/More Forms/moreforms/src/components/UserForm.js
+++ SOFTWARE WEB DEVELOPMENT - MERN/React/More Forms/moreforms/src/components/UserForm.js
@@ -1,43 +1,88 @@
 import React, { useState } from 'react';
 
 const UserForm = (props) => {
-    const [fname, setFirstname] = useState("");
-    const [Firstname, setFirstnameError] = useState("");
-    const handleFirstname = (e) => {
-        setFirstname(e.target.value);
-        if (e.target.value.length < 1) {
-            setFirstname("Title is required!");
-        } else if (e.target.value.length < 3) {
-            setFirstname("Title must be 3 characters or longer!");
-        }
-    };
-    const [lname, setLastname] = useState("");
-    const [email, setEmail] = useState("");
+
+    // const [lname, setLastname] = useState("");
+    const [email, setEmailError] = useState("");
     const [password, setPassword] = useState("");
     const [passwordCon, setPasswordConform] = useState("");
-    const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
-
-    const createUser = (e) => {
-        e.preventDefault();
-        const newUser = { fname, lname, email, password, passwordCon };
-        console.log("Welcome", newUser);
-        setHasBeenSubmitted(true);
-    };
+    // const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
+    const [Firstname, setFirstnameError] = useState("");
+    const [lastname, setLastnameError] = useState("");
+    const [passwordConError, setPasswordConError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [MatchError, setMatchError] = useState("");
 
 
-
-    const formMessage = () => {
-        if (hasBeenSubmitted) {
-            return "Thank you for submitting the form!";
+    const handleFirstname = (e) => {
+        // setFirstnameError(e.target.value);
+        if (e.target.value.length < 2) {
+            setFirstnameError("First name must be at least 2 characters");
         } else {
-            return "Welcome, please submit the form";
+            setFirstnameError(" ");
         }
     };
+    const handleLastname = (e) => {
+        // setLastnameError(e.target.value);
+        if (e.target.value.length < 2) {
+            setLastnameError("Last name must be at least 2 characters");
+        } else {
+            setLastnameError(" ");
+        }
+    };
+    const handleEmail = (e) => {
+        // setEmailError(e.target.value);
+        if (e.target.value.length < 5) {
+            setEmailError("Email must be at least 5 characters");
+        } else {
+            setEmailError(" ");
+        }
+    };
+
+    const passwordhandel = (e) => {
+        if (e.target.value.length < 8) {
+            setPasswordError("password must be at least 8 characters");
+        } else {
+            setPasswordError(" ");
+
+            setPassword(e.target.value);
+            //console.log(password)
+            handlepassword()
+        }
+
+    };
+
+    const passwordConhandle = (e) => {
+        if (e.target.value.length < 8) {
+            setPasswordConError("password must be at least 8 characters");
+        } else {
+            setPasswordConError(" ");
+            setPasswordConform(e.target.value);
+            // console.log(passwordCon)
+            handlepassword()
+        }
+
+    };
+
+    const handlepassword = () => {
+        console.log(passwordCon)
+        console.log(password)
+        if (password === passwordCon) {
+            setMatchError(" ");
+        }
+        else {
+            setMatchError("Password must match");
+        }
+    };
+
+
+
+
 
     return (
         <div className="container m-5">
             <div>
-                <form onSubmit={createUser}>
+                <form>
 
                     <div className="form-group my-2">
                         <label>First Name: </label>
@@ -45,46 +90,57 @@ const UserForm = (props) => {
                         {
                             Firstname ?
                                 <p style={{ color: 'red' }}>{Firstname}</p> :
-                                ''
+                                'First name must be at least 2 characters'
                         }
                     </div>
                     <div className="form-group my-2">
                         <label>Last Name: </label>
-                        <input className='form-control' type="text" onChange={(e) => setLastname(e.target.value)} />
+                        <input className='form-control' type="text" onChange={handleLastname} />
                         {
-                            hasBeenSubmitted ?
-                                <p>Thank you for submitting the form!</p> :
-                                <p>Welcome, please submit the form.</p>
+                            lastname ?
+                                <p style={{ color: 'red' }}>{lastname}</p> :
+                                'Last name must be at least 2 characters'
                         }
                     </div>
                     <div className="form-group my-2">
                         <label>Email Address: </label>
-                        <input className='form-control' type="text" onChange={(e) => setEmail(e.target.value)} />
+                        <input className='form-control' type="email" onChange={handleEmail} />
                         {
-                            hasBeenSubmitted ?
-                                <p>Thank you for submitting the form!</p> :
-                                <p>Welcome, please submit the form.</p>
+                            email ?
+                                <p style={{ color: 'red' }}>{email}</p> :
+                                'Email must be at least 5 characters'
                         }
                     </div>
-                    <div className="form-group my-2">
-                        <label>Confirm Password: </label>
-                        <input className='form-control' type="password" onChange={(e) => setPasswordConform(e.target.value)} />
-                        {
-                            hasBeenSubmitted ?
-                                <p>Thank you for submitting the form!</p> :
-                                <p>Welcome, please submit the form.</p>
-                        }
-                    </div>
+
                     <div className="form-group my-2">
                         <label>Password: </label>
-                        <input className='form-control' type="password" onChange={(e) => setPassword(e.target.value)} />
-                        {
-                            hasBeenSubmitted ?
-                                <p>Thank you for submitting the form!</p> :
-                                <p>Welcome, please submit the form.</p>
+                        <input className='form-control' type="text" onChange={passwordhandel} />
+                        <p>{
+                            passwordError ?
+                                <p style={{ color: 'red' }}>{passwordError}</p> :
+                                <p>Password must be at least 8 characters</p>
                         }
+                        </p>
                     </div>
-                    <input type="submit" value="Create User" />
+
+                    <div className="form-group my-2">
+                        <label>Confirm Password: </label>
+                        <input className='form-control' type="text" onChange={passwordConhandle} />
+                        <p>{
+                            passwordConError ?
+                                <p style={{ color: 'red' }}>{passwordConError}</p> :
+                                <p>Password must be at least 8 characters</p>
+                        }
+                        </p>
+                        <p>{
+                            MatchError ?
+                                <p style={{ color: 'red' }}>{MatchError}</p> :
+                                <p></p>
+                        }
+                        </p>
+
+
+                    </div>
                 </form>
             </div>
         </div>

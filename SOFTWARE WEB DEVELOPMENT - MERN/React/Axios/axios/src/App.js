1ecfbf08c2b58e887a0179329685a5d2247e57b4
esraa alsharit <esraa.alsharit@gmail.com>
2022-08-25 17:42:57 +0300

    Axios

---
+++ SOFTWARE WEB DEVELOPMENT - MERN/React/Axios/axios/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Axios from './Components/Axios';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Axios />
+    </div>
+  );
+}
+
+export default App;
